de4919e93bfd0af98d65f948724ff82145f2accb
Yo sup <dhdytjq2@gamil.com>
2023-05-20 15:00:23 +0900

    cra-bundle-analyzer

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './index.css'
+import { stringify } from 'superjson'
 
 function zeroPad(value, len) {
   const str = '0000000000' + value.toString()
@@ -17,22 +18,27 @@ function getParametersForUnsplash({width, height, quality, format}) {
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
 function removeSpecialCharacter(str) {
-  const removeCharacters = ['#', '_', '*', '~', '&', ';', '!', '[', ']', '`', '>', '\n', '=', '-']
-  let _str = str
-  let i = 0,
-    j = 0
 
-  for (i = 0; i < removeCharacters.length; i++) {
-    j = 0
-    while (j < _str.length) {
-      if (_str[j] === removeCharacters[i]) {
-        _str = _str.substring(0, j).concat(_str.substring(j + 1))
-        continue
-      }
-      j++
-    }
-  }
+  // const removeCharacters = ['#', '_', '*', '~', '&', ';', '!', '[', ']', '`', '>', '\n', '=', '-']
+  // let _str = str
+  // let i = 0,
+  //   j = 0
 
+  // for (i = 0; i < removeCharacters.length; i++) {
+  //   j = 0
+  //   while (j < _str.length) {
+  //     if (_str[j] === removeCharacters[i]) {
+  //       _str = _str.substring(0, j).concat(_str.substring(j + 1))
+  //       continue
+  //     }
+  //     j++
+  //   }
+  // }
+
+  // return _str
+
+  let _str = str.substring(0, 300)  
+  _str = _str.replace(/[\#\_\*\~\&\;\!\[\]\`\>\/n\=\-]/g, "")
   return _str
 }
 
@@ -52,7 +58,7 @@ function Article(props) {
         </div>
       </div>
       <div className={'Article__thumbnail'}>
-        <img src={props.image + getParametersForUnsplash({width: 1200, height: 1200, quality: 80, format: 'jpg'})} alt="thumbnail" />
+        <img src={props.image + getParametersForUnsplash({width: 240, height: 240, quality: 80, format: 'jpg'})} alt="thumbnail" />
       </div>
     </div>
   )
